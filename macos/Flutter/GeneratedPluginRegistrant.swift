2b204bdb311225107e183d397a8e2b6ce9589420
I m r a a n <imrn.dev@gmail.com>
2026-08-16 12:06:59 +0530

    chore: regenerate desktop plugin registrants
    Picks up flutter_local_notifications and flutter_timezone for the
    desktop build targets (unused there, but the registrant must list
    every plugin regardless of platform).

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,13 @@ import FlutterMacOS
 import Foundation
 
 import connectivity_plus
+import flutter_local_notifications
 import flutter_secure_storage_darwin
+import flutter_timezone
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
+  FlutterTimezonePlugin.register(with: registry.registrar(forPlugin: "FlutterTimezonePlugin"))
 }
